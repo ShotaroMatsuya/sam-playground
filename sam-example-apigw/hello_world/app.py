5de4902dfaafbefa952da29440f0498a4ba4a3c7
ShotaroMatsuya <68782833+ShotaroMatsuya@users.noreply.github.com>
2022-10-18 22:29:24 +0900

    CREATE: lambda func & cfn tmpl

--- sam-example-apigw/hello_world/app.py
+++ sam-example-apigw/hello_world/app.py
@@ -1,42 +1,121 @@
+import base64
+import decimal
 import json
+import os
+import time
 
-# import requests
+import boto3
+
+# DynamoDBオブジェクト
+dynamodb = boto3.resource('dynamodb')
+# S3オブジェクト
+s3 = boto3.client('s3')
+# SESオブジェクト
+client = boto3.client('ses')
+
+MAILFROM = os.environ['MAILFROM']
+
+
+def sendmail(to, subject, body):
+    client.send_email(
+        Source=MAILFROM,
+        ReplyToAddress=[MAILFROM],
+        Destination={
+            'ToAddresses': [
+                to
+            ]
+        },
+        Message={
+            'Subject': {
+                'Data': subject,
+                'Charset': 'UTF-8',
+            },
+            'Body': {
+                'Text': {
+                    'Data': body,
+                    'Charset': 'UTF-8'
+                }
+            }
+        }
+    )
+
+
+# 連番更新
+def next_seq(table, tablename):
+    response = table.update_item(
+        Key={
+            'tablename': tablename
+        },
+        UpdateExpression="set seq = seq + :val",
+        ExpressionAttributeValues={
+            ':val': 1
+        },
+        ReturnValues='UPDATE_NEW'
+    )
+    return response['Attributes']['seq']
 
 
 def lambda_handler(event, context):
-    """Sample pure Lambda function
+    try:
+        # シーケンスデータを取得
+        seqtable = dynamodb.Table(os.environ['SEQUENCETABLE'])
+        nextseq = next_seq(seqtable, os.environ['USERTABLE'])
+        body = event['body']
+        if event['isBase64Encoded']:
+            body = base64.b64decode(body)
+            decoded = json.loads(body)
+            username = decoded['username']
+            email = decoded['email']
+            # クライアントのIPを得る
+            host = event['requestContext']['http']['sourceIp']
 
-    Parameters
-    ----------
-    event: dict, required
-        API Gateway Lambda Proxy Input Format
-
-        Event doc: https://docs.aws.amazon.com/apigateway/latest/developerguide/set-up-lambda-proxy-integrations.html#api-gateway-simple-proxy-for-lambda-input-format
-
-    context: object, required
-        Lambda Context runtime methods and attributes
-
-        Context doc: https://docs.aws.amazon.com/lambda/latest/dg/python-context-object.html
-
-    Returns
-    ------
-    API Gateway Lambda Proxy Output Format: dict
-
-        Return doc: https://docs.aws.amazon.com/apigateway/latest/developerguide/set-up-lambda-proxy-integrations.html
-    """
-
-    # try:
-    #     ip = requests.get("http://checkip.amazonaws.com/")
-    # except requests.RequestException as e:
-    #     # Send some context about this error to Lambda Logs
-    #     print(e)
-
-    #     raise e
-
-    return {
-        "statusCode": 200,
-        "body": json.dumps({
-            "message": "hello world",
-            # "location": ip.text.replace("\n", "")
-        }),
-    }
+            now = time.time()
+            
+            # 署名付きURLを作成
+            url = s3.generate_presigned_url(
+                ClientMethod='get_object',
+                Params={
+                    'Bucket': os.environ['SAVEBUCKET'],
+                    'Key': 'special.pdf'
+                },
+                ExpiresIn=8 * 60 * 60,
+                HttpMethod='GET' 
+            )
+            
+            # usertableに登録
+            usertable = dynamodb.Table('user')
+            usertable.put_item(
+                Item={
+                    'id': nextseq,
+                    'username': username,
+                    'email': email,
+                    'accepted_at': decimal.Decimal(str(now)),
+                    'host': host,
+                    'url': url
+                }
+            )
+            
+            # メール送信
+            mailbody = """"
+{0}様
+ご登録ありがとうございました。
+下記のURLからダウンロードできます。
+{1}
+""".format(username, url)
+            sendmail(email, "登録ありがとうございました", mailbody)
+            
+            return json.dumps({})
+    except Exception:
+        import traceback
+        err = traceback.format_exc()
+        print(err)
+        
+        return {
+            'statusCode': 500,
+            'headers': {
+                'content-type': 'text/json'
+            },
+            'body': json.dumps({
+                'error': '内部エラーが発生しました'
+            })
+        }
