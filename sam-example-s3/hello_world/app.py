e9b387fa43ef8eb32398ef0f3d330f648f41c413
ShotaroMatsuya <68782833+ShotaroMatsuya@users.noreply.github.com>
2022-10-18 13:07:20 +0900

    UPDATE: upload s3 for outputs

--- sam-example-s3/hello_world/app.py
+++ sam-example-s3/hello_world/app.py
@@ -1,9 +1,28 @@
-# import json
+import os
+import tempfile
 
-# import requests
+import boto3
 
 
 def lambda_handler(event, context):
+    s3 = boto3.resource('s3')
+    tmpdir = tempfile.TemporaryDirectory()
     for rec in event['Records']:
-        print('filename=' + rec['s3']['object']['key'])
-        print('bucketname=' + rec['s3']['bucket']['name'])
+        filename = rec['s3']['object']['key']
+        bucketname = rec['s3']['bucket']['name']
+
+        obj = s3.Object(bucketname, filename)
+        # ファイルの情報取得
+        response = obj.get()
+        localfilename = os.path.join(tmpdir.name, filename)
+        fp = open(localfilename, 'wb')
+        fp.write(response['Body'].read())
+        fp.close()
+
+        destbucketname = os.environ['OUTPUTBUCKET']
+        obj2 = s3.Object(destbucketname, filename)
+        obj2.put(
+            Body=open(localfilename, 'rb')
+        )
+        
+        tmpdir.cleanup()
